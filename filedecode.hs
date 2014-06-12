93ccc9e754048257fd96727bf1141be14a576f85
Mathieu Suen <mathk.sue@gmail.com>
2014-06-12 16:30:00 +0200

    Add parser from Real World Haskell

--- filedecode.hs
+++ filedecode.hs
@@ -4,16 +4,66 @@ import qualified Data.ByteString.Char8 as C
 import qualified Data.Binary.Strict.Get as G
 import qualified Data.Binary.Strict.BitGet as BG
 import Data.Binary.Strict.Util
+import Data.Int (Int64)
 import Text.Printf
 import Data.Word
 
+data ParseState = ParseState {
+        string :: B.ByteString,
+        offset :: Int64
+    } deriving (Show)
+
 data ELF_Header_Magic = ELF_Header_Magic Word8 String
+
+instance Show ELF_Header_Magic where
+    show (ELF_Header_Magic w s) = printf "0x%02X %s" w s
+
+newtype Parse a = Parse {
+        runParse :: ParseState -> Either String (a, ParseState)
+    }
+
+getState :: Parse ParseState
+getState = Parse (\s -> Right (s, s))
+
+putState :: ParseState -> Parse ()
+putState s = Parse (\_ -> Right((), s))
+
+(==>) :: Parse a -> (a -> Parse b) -> Parse b
+firstParser ==> secondParser = Parse chainedParser
+    where chainedParser initState = 
+            case runParse firstParser initState of
+                Left errMessage -> Left errMessage
+                Right (firstResult, newState) ->
+                    runParse (secondParser firstResult) newState
+
+bail :: String -> Parse a
+bail err = Parse $ \s -> Left $ "byte offset" ++ show (offset s) ++ ": " ++ err
+
+identity :: a -> Parse a
+identity a = Parse (\s -> Right (a, s))
+
+parseByte :: Parse Word8
+parseByte =
+    getState ==> \initState ->
+        case B.uncons (string initState) of
+            Nothing -> bail "no more input"
+            Just (byte, remainder) ->
+                  Parse (\_ -> Right(byte, newState))
+             where newState = initState { string = remainder,
+                                             offset = newOffset }
+                   newOffset = offset initState + 1
+
+parse :: Parse a -> B.ByteString -> Either String a
+parse parser input =
+    case runParse parser (ParseState input 0) of
+        Left err    ->Left err
+        Right (result, _) -> Right result
 
 parseElfHeaderMagic :: G.Get ELF_Header_Magic
 parseElfHeaderMagic = do 
-	magicByte <- G.getWord8 
-	magicString <- G.getByteString 3
-	return $ ELF_Header_Magic magicByte (C.unpack magicString)
+    magicByte <- G.getWord8 
+    magicString <- G.getByteString 3
+    return $ ELF_Header_Magic magicByte (C.unpack magicString)
 
 parseElfClass :: G.Get Word8
 parseElfClass = G.getWord8
@@ -22,26 +72,16 @@ parseElfClass = G.getWord8
 (Right r, c) >>? f  = f c r
 (Left a, c) >>? f = do return (Left a, c) 
 
-parse :: B.ByteString -> IO (Either String Word8, B.ByteString)
-parse input = G.runGet parseElfHeaderMagic input >>? 
-	(\c (ELF_Header_Magic w t) -> do 
-		putStrLn $printf "0x%02X" w
-		return $ G.runGet parseElfClass c)
+oldparse :: B.ByteString -> IO (Either String Word8, B.ByteString)
+oldparse input = G.runGet parseElfHeaderMagic input >>? 
+    (\c h -> do 
+        putStrLn $ show h
+        return $ G.runGet parseElfClass c)
 
 main = do 
-	input <- B.readFile "linker"
-	(a, _) <- parse input
-	case a of 
-		Right _ -> putStrLn "Parse succeed"
-		Left d -> putStrLn d
-{-	let r = G.runGet parseElfHeaderMagic input 
-	case fst r of
-		Right (ELF_Header_Magic magic0 magic1) -> do 
-			putStrLn $ printf "0x%02X" magic0
-			putStrLn magic1
-			case fst $ G.runGet parseElfClass (snd r) of
-				Right a -> putStrLn $ printf "%d" a
-				Left a -> putStrLn a
-		Left a -> putStrLn a
-	
--}
+    input <- B.readFile "linker"
+    (a, _) <- oldparse input
+    case a of 
+        Right _ -> putStrLn "Parse succeed"
+        Left d -> putStrLn d
+
