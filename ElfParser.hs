f27d71659feb569f84b66a1f1138686a87bbd269
Mathieu Suen <mathk.sue@gmail.com>
2014-06-18 00:44:09 +0200

    Working on split ELF parsing from file parsing

--- ElfParser.hs
+++ ElfParser.hs
@@ -5,6 +5,163 @@ module ElfParser
 
 import FileDecoding
 
+{- ELF Data type -}
+data ELFHeaderMagic = ELFHeaderMagic Word8 String
+
+data ELFHeaderClass = ELF32 | ELF64
+
+data ELFHeaderData = ELFBigEndian | ELFLittleEndian
+
+data ELFHeaderVersion = ELFDefaultVersion | ELFOtherVersion
+
+data ELFHeaderABI = ELFHeaderABI Word8
+
+data ELFHeaderType = ELFRelocatable | ELFExecutable | ELFShared | ELFCore
+
+data ELFHeaderMachine = 
+        ELFSPARC    |
+        ELFx86      |
+        ELFMIPS     |
+        ELFPowerPC  |
+        ELFARM      |
+        ELFSuperH   |
+        ELFIA64     |
+        ELFx86_64   |
+        ELFAArch64
+
+data ELFHeader = ELFHeader {
+        magic :: ELFHeaderMagic,
+        format :: ELFHeaderClass,
+        endianness :: ELFHeaderData,
+        version :: ELFHeaderVersion,
+        osabi :: ELFHeaderABI,
+        objectType :: ELFHeaderType,
+        machine ::  ELFHeaderMachine,
+        entry :: Address,
+        phoff :: Address,
+        shoff :: Address,
+        flags :: Word32
+    }
+
+{- ELF instances -}
+newtype Address = Address (Either Word32 Word64)
+
+instance Show Address where
+    show (Address (Left w)) = printf "0x%08X" w
+    show (Address (Right w)) = printf "0x%016X" w
+
+instance Show ELFHeaderMagic where
+    show (ELFHeaderMagic w s) = printf "0x%02X %s" w s
+
+instance Show ELFHeaderClass where
+    show ELF32 = "32bit app"
+    show ELF64 = "64bit app"
+
+instance Show ELFHeaderVersion where
+    show ELFDefaultVersion = "Original"
+    show ELFOtherVersion   = "Other"
+
+
+instance Show ELFHeaderType where
+    show ELFRelocatable = "relocatable"
+    show ELFExecutable = "executalbe"
+    show ELFShared = "shared"
+    show ELFCore = "core"
+
+instance Show ELFHeaderMachine where
+    show ELFSPARC   = "SPARC"
+    show ELFx86     = "x86"
+    show ELFMIPS    = "MIPS"
+    show ELFPowerPC = "PowerPC"
+    show ELFARM     = "ARM"
+    show ELFSuperH  = "SuperH"
+    show ELFIA64    = "IA-64"
+    show ELFx86_64  = "x86-64"
+    show ELFAArch64 = "AArch64"
+
+instance Show ELFHeader where
+    show ELFHeader { magic=m, format=c, endianness=e, version=v, osabi=abi, objectType=t, machine=arch, entry=ent, phoff=ph, shoff=sh, flags=f} =
+        printf "Magic: %s\nClass: %s\nEndianness: %s\nVersion: %s\nOSABI: %s\nType: %s\nMachine: %s\nEntry point: %s\nPhoff: %s\nShoff: %s\nFlags: 0x%08X"
+            (show m)
+            (show c)
+            (show e)
+            (show v)
+            (show abi)
+            (show t)
+            (show arch)
+            (show ent)
+            (show ph)
+            (show sh)
+            f
+
+instance Show ELFHeaderABI where
+    show (ELFHeaderABI abi) = printf "ABI(0x%02X)" abi
+
+{- ELf specific routine -}
+parseELFHeaderClass :: Parse ELFHeaderClass
+parseELFHeaderClass = parseByte ==> \b ->
+        case b of
+            1 -> getState ==> \state -> 
+                 putState state {size = ELF32} ==>& identity ELF32
+            2 -> getState ==> \state ->
+                 putState state {size = ELF64} ==>& identity ELF64
+            _ -> bail $ printf "Unknown class (0x02X)" b
+
+parseELFHeaderMagic :: Parse ELFHeaderMagic
+parseELFHeaderMagic = parseByte ==> \magicByte ->
+        assert (magicByte == 0x7F) "First magic byte is wrong" ==>&
+        parseIdentifier ==> \ident ->
+            assert (ident == "ELF") "Magic string is not ELF" ==>&
+            identity (ELFHeaderMagic magicByte ident)
+
+parseELFHeaderEndianness :: Parse Endianness
+parseELFHeaderEndianness = parseByte ==> \b ->
+            case b of
+                1 -> getState ==> \state ->
+                     putState state {stateEndianness = LittleEndian } ==>& identity LittleEndian
+                0 -> getState ==> \state ->
+                     putState state {stateEndianness = BigEndian} ==>& identity BigEndian
+                _ -> bail $ printf "Bad endianness (0x%02X)" b
+
+parseELFHeaderType :: Parse ELFHeaderType
+parseELFHeaderType = parseHalf ==> \b ->
+        case b of
+            1 -> identity ELFRelocatable
+            2 -> identity ELFExecutable
+            3 -> identity ELFShared
+            4 -> identity ELFCore
+            _ -> bail $ printf "Bad elf type (0x%02X)" b
+
+parseELFHeaderMachine :: Parse ELFHeaderMachine
+parseELFHeaderMachine = parseHalf ==> \b ->
+        case b of
+            0x02 -> identity ELFSPARC
+            0x03 -> identity ELFx86
+            0x08 -> identity ELFMIPS
+            0x14 -> identity ELFPowerPC
+            0x28 -> identity ELFARM
+            0x2A -> identity ELFSuperH
+            0x32 -> identity ELFIA64
+            0x3E -> identity ELFx86_64
+            0xB7 -> identity ELFAArch64
+            _ -> bail $ printf "Unknown machine (0x%02X)" b
+
+parseELFHeaderVersion :: Parse ELFHeaderVersion
+parseELFHeaderVersion = parseByte ==> \b ->
+        case b of
+            1 -> identity ELFDefaultVersion
+            _ -> identity ELFOtherVersion
+
+parseELFHeaderABI :: Parse ELFHeaderABI
+parseELFHeaderABI = parseByte ==> \b ->
+        identity (ELFHeaderABI b)
+
+parseELFAddress :: Parse Address
+parseELFAddress = getState ==> \state ->
+    case size state of
+        ELF32 -> parseWord ==> \w -> identity $ Address (Left w)
+        ELF64 -> parseGWord ==> \w -> identity $ Address (Right w)
+
 parseELFHeader :: Parse ELFHeader
 parseELFHeader = parseELFHeaderMagic ==> \m ->
         parseELFHeaderClass ==> \f ->
--- FileDecoding.hs
+++ FileDecoding.hs
@@ -1,5 +1,9 @@
 module FileDecoding
     (
+      Parse(..),
+      Endianness(..),
+      (==>),
+      (==>&),
       parse
     ) where
 
@@ -17,107 +21,19 @@ import Data.Word
 data ParseState = ParseState {
         string :: B.ByteString,
         offset :: Int64,
-        stateEndianness :: ELFHeaderData,
+        stateEndianness :: Endianness,
         size :: ELFHeaderClass
     } deriving (Show)
 
-data ELFHeaderMagic = ELFHeaderMagic Word8 String
-
-data ELFHeaderClass = ELF32 | ELF64
-
-data ELFHeaderData = ELFBigEndian | ELFLittleEndian
-
-data ELFHeaderVersion = ELFDefaultVersion | ELFOtherVersion
-
-data ELFHeaderABI = ELFHeaderABI Word8
-
-data ELFHeaderType = ELFRelocatable | ELFExecutable | ELFShared | ELFCore
-
-data ELFHeaderMachine = 
-        ELFSPARC    |
-        ELFx86      |
-        ELFMIPS     |
-        ELFPowerPC  |
-        ELFARM      |
-        ELFSuperH   |
-        ELFIA64     |
-        ELFx86_64   |
-        ELFAArch64
-
-data ELFHeader = ELFHeader {
-        magic :: ELFHeaderMagic,
-        format :: ELFHeaderClass,
-        endianness :: ELFHeaderData,
-        version :: ELFHeaderVersion,
-        osabi :: ELFHeaderABI,
-        objectType :: ELFHeaderType,
-        machine ::  ELFHeaderMachine,
-        entry :: Address,
-        phoff :: Address,
-        shoff :: Address,
-        flags :: Word32
-    }
+data Endianness = BigEndian | LittleEndian
 
 newtype Parse a = Parse {
         runParse :: ParseState -> Either String (a, ParseState)
     }
 
-newtype Address = Address (Either Word32 Word64)
-
-instance Show Address where
-    show (Address (Left w)) = printf "0x%08X" w
-    show (Address (Right w)) = printf "0x%016X" w
-
-instance Show ELFHeaderMagic where
-    show (ELFHeaderMagic w s) = printf "0x%02X %s" w s
-
-instance Show ELFHeaderClass where
-    show ELF32 = "32bit app"
-    show ELF64 = "64bit app"
-
-instance Show ELFHeaderVersion where
-    show ELFDefaultVersion = "Original"
-    show ELFOtherVersion   = "Other"
-
-
-instance Show ELFHeaderType where
-    show ELFRelocatable = "relocatable"
-    show ELFExecutable = "executalbe"
-    show ELFShared = "shared"
-    show ELFCore = "core"
-
-instance Show ELFHeaderMachine where
-    show ELFSPARC   = "SPARC"
-    show ELFx86     = "x86"
-    show ELFMIPS    = "MIPS"
-    show ELFPowerPC = "PowerPC"
-    show ELFARM     = "ARM"
-    show ELFSuperH  = "SuperH"
-    show ELFIA64    = "IA-64"
-    show ELFx86_64  = "x86-64"
-    show ELFAArch64 = "AArch64"
-
-instance Show ELFHeader where
-    show ELFHeader { magic=m, format=c, endianness=e, version=v, osabi=abi, objectType=t, machine=arch, entry=ent, phoff=ph, shoff=sh, flags=f} =
-        printf "Magic: %s\nClass: %s\nEndianness: %s\nVersion: %s\nOSABI: %s\nType: %s\nMachine: %s\nEntry point: %s\nPhoff: %s\nShoff: %s\nFlags: 0x%08X"
-            (show m)
-            (show c)
-            (show e)
-            (show v)
-            (show abi)
-            (show t)
-            (show arch)
-            (show ent)
-            (show ph)
-            (show sh)
-            f
-
-instance Show ELFHeaderData where
-    show ELFLittleEndian = "Little Endian"
-    show ELFBigEndian    = "Big Endian"
-
-instance Show ELFHeaderABI where
-    show (ELFHeaderABI abi) = printf "ABI(0x%02X)" abi
+instance Show Endianness where
+    show LittleEndian = "Little Endian"
+    show BigEndian    = "Big Endian"
 
 {- Parser composition -}
 (==>) :: Parse a -> (a -> Parse b) -> Parse b
@@ -178,11 +94,11 @@ parseByte =
 parseHalf :: Parse Word16
 parseHalf = getState ==> \state ->
     case stateEndianness state of
-        ELFLittleEndian ->
+        LittleEndian ->
             parseByte ==> \lessByte ->
             parseByte ==> \mostByte ->
                 identity $ w8tow16 mostByte lessByte
-        ELFBigEndian ->
+        BigEndian ->
             parseByte ==> \mostByte ->
             parseByte ==> \lessByte ->
                 identity $ w8tow16 mostByte lessByte
@@ -190,11 +106,11 @@ parseHalf = getState ==> \state ->
 parseWord :: Parse Word32
 parseWord = getState ==> \state ->
     case stateEndianness state of
-        ELFLittleEndian ->
+        LittleEndian ->
             parseHalf ==> \lessBytes ->
             parseHalf ==> \mosteBytes ->
                 identity $ w16tow32 mosteBytes lessBytes
-        ELFBigEndian ->
+        BigEndian ->
             parseHalf ==> \mosteBytes ->
             parseHalf ==> \lessBytes ->
                 identity $ w16tow32 mosteBytes lessBytes
@@ -202,11 +118,11 @@ parseWord = getState ==> \state ->
 parseGWord :: Parse Word64
 parseGWord = getState ==> \state ->
     case stateEndianness state of 
-        ELFLittleEndian ->
+        LittleEndian ->
             parseWord ==> \lessWord ->
             parseWord ==> \mosteWord ->
                 identity $ w32tow64 mosteWord lessWord
-        ELFBigEndian ->
+        BigEndian ->
             parseWord ==> \mosteWord ->
             parseWord ==> \lessWord ->
                 identity $ w32tow64 mosteWord lessWord
@@ -245,75 +161,10 @@ assert :: Bool -> String -> Parse ()
 assert True  _   = identity ()
 assert False err = bail err
 
-{- ELf specific routine -}
-parseELFHeaderClass :: Parse ELFHeaderClass
-parseELFHeaderClass = parseByte ==> \b ->
-        case b of
-            1 -> getState ==> \state -> 
-                 putState state {size = ELF32} ==>& identity ELF32
-            2 -> getState ==> \state ->
-                 putState state {size = ELF64} ==>& identity ELF64
-            _ -> bail $ printf "Unknown class (0x02X)" b
-
-parseELFHeaderMagic :: Parse ELFHeaderMagic
-parseELFHeaderMagic = parseByte ==> \magicByte ->
-        assert (magicByte == 0x7F) "First magic byte is wrong" ==>&
-        parseIdentifier ==> \ident ->
-            assert (ident == "ELF") "Magic string is not ELF" ==>&
-            identity (ELFHeaderMagic magicByte ident)
-
-parseELFHeaderEndianness :: Parse ELFHeaderData
-parseELFHeaderEndianness = parseByte ==> \b ->
-            case b of
-                1 -> getState ==> \state ->
-                     putState state {stateEndianness = ELFLittleEndian } ==>& identity ELFLittleEndian
-                0 -> getState ==> \state ->
-                     putState state {stateEndianness = ELFBigEndian} ==>& identity ELFBigEndian
-                _ -> bail $ printf "Bad endianness (0x%02X)" b
-
-parseELFHeaderType :: Parse ELFHeaderType
-parseELFHeaderType = parseHalf ==> \b ->
-        case b of
-            1 -> identity ELFRelocatable
-            2 -> identity ELFExecutable
-            3 -> identity ELFShared
-            4 -> identity ELFCore
-            _ -> bail $ printf "Bad elf type (0x%02X)" b
-
-parseELFHeaderMachine :: Parse ELFHeaderMachine
-parseELFHeaderMachine = parseHalf ==> \b ->
-        case b of
-            0x02 -> identity ELFSPARC
-            0x03 -> identity ELFx86
-            0x08 -> identity ELFMIPS
-            0x14 -> identity ELFPowerPC
-            0x28 -> identity ELFARM
-            0x2A -> identity ELFSuperH
-            0x32 -> identity ELFIA64
-            0x3E -> identity ELFx86_64
-            0xB7 -> identity ELFAArch64
-            _ -> bail $ printf "Unknown machine (0x%02X)" b
-
-parseELFHeaderVersion :: Parse ELFHeaderVersion
-parseELFHeaderVersion = parseByte ==> \b ->
-        case b of
-            1 -> identity ELFDefaultVersion
-            _ -> identity ELFOtherVersion
-
-parseELFHeaderABI :: Parse ELFHeaderABI
-parseELFHeaderABI = parseByte ==> \b ->
-        identity (ELFHeaderABI b)
-
-parseELFAddress :: Parse Address
-parseELFAddress = getState ==> \state ->
-    case size state of
-        ELF32 -> parseWord ==> \w -> identity $ Address (Left w)
-        ELF64 -> parseGWord ==> \w -> identity $ Address (Right w)
-
 {- Parse engine that chain all the parser -}
 parse :: Parse a -> B.ByteString -> Either String a
 parse parser input =
-    case runParse parser (ParseState input 0 ELFLittleEndian ELF32) of
+    case runParse parser (ParseState input 0 LittleEndian ELF32) of
         Left err            -> Left err
         Right (result, _)   -> Right result
 
