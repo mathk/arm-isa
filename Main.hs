f96b9e3b9403c98697f5ea1c33e7ab2f2be7dbaf
Mathieu Suen <mathk.sue@gmail.com>
2014-06-17 18:42:31 +0200

    Refactor according to hlint

--- Main.hs
+++ Main.hs
@@ -5,6 +5,6 @@ main :: IO ()
 main = do
     input <- B.readFile "linker"
     case parse parseELFHeader  input of
-        Right value -> putStrLn $ show value
+        Right value -> print value
         Left d -> putStrLn d
  
