0be0a66dcbb59e7659f001d38079c25d186f8715
Mathieu Suen <mathk.sue@gmail.com>
2014-06-22 19:01:28 +0200

    Improve argument reading

--- Main.hs
+++ Main.hs
@@ -5,25 +5,31 @@ import System.Exit (ExitCode(..), exitWith)
 import System.IO
 import System.Console.GetOpt
 
-data Options = GetSectionHeader | GetHeader | GetProgramHeader
+data Options = GetSectionHeader | GetHeader | GetProgramHeader | GetHelp
 
 option :: [OptDescr Options]
 option = [ Option ['p'] ["program-headers"] (NoArg GetProgramHeader) "List program headers",
            Option ['s'] ["section-headers"] (NoArg GetSectionHeader) "List section headers",
-           Option ['e'] ["header"] (NoArg GetHeader) "Shoe the header"]
+           Option ['e'] ["header"] (NoArg GetHeader) "Show the elf header",
+           Option ['h'] ["help"] (NoArg GetHelp) "Show this message"]
 
 parseArgs :: IO Options
 parseArgs = do
     argv <- getArgs
+    name <- getProgName
     case parse argv of  
         ([GetProgramHeader], file, []) -> return GetProgramHeader
         ([GetSectionHeader], file, []) -> return GetSectionHeader
         ([GetHeader], file,  []) -> return GetHeader
-        (_, _, errs) -> die $ concat errs
+        ([GetHelp], _, _) -> help name
+        (_, _, errs) -> die errs name
   where
-    parse = getOpt Permute option
-    dump = hPutStrLn stderr
-    die errs = dump errs >> exitWith (ExitFailure 1)
+    parse           = getOpt Permute option
+    header name     = "Usage: " ++ name ++ " [-hpse]"
+    info name       = usageInfo (header name) option
+    dump            = hPutStrLn stderr
+    die errs name   = dump (concat errs ++ (info name)) >> exitWith (ExitFailure 1)
+    help name       = dump (info name) >> exitWith ExitSuccess
     
 
 main :: IO ()
